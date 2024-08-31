9c447a759f48023158f0a0f368c3f2dfbedf82ee
Rafael Rodrigues <rodriguesra@hotmail.com>
2024-08-31 13:26:17 -0300

    feat: add Transactions context

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components";
+import { TransactionsProvider } from "./contexts/TransactionsContext";
 import { Transactions } from "./pages/Transactions";
 import GlobalStyles from "./styles/global.ts";
 import { defaultTheme } from "./styles/themes/default.ts";
@@ -7,7 +8,9 @@ function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
-      <Transactions />
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
     </ThemeProvider>
   );
 }
